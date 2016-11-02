9a7b07791e03355952949dfebb0d49ca2b6d8bbf
bilashsaha03 <bilashsaha03@gmail.com>
2016-11-03 00:01:04 +0600

    added tick and some scope

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,3 +1,10 @@
 class Token < ActiveRecord::Base
   belongs_to :sanchaypatra
+
+  scope :redeemed, -> { where(is_redeemed: true) }
+  scope :not_redeemed, -> { where(is_redeemed: false) }
+  scope :till_date, -> (to_date) { where("token_date < ?",to_date) }
+
+
+
 end
